47b5d3212da3d5eecc8e73369e2c65df6d7938ee
tal <tal32123@gmail.com>
2016-09-27 06:18:03 -0500

    Make free and paid project flavors

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -3,7 +3,6 @@ package com.udacity.gradle.builditbigger;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.JokeifyMe;
@@ -27,20 +26,7 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     JokeifyMe jokeifyMe = new JokeifyMe();
 
     public void tellJoke(View view) {
